ca08ddb7bb1055956ccae4eb3753ae8ad5fb3cf9
FRJohan312 <johan37021@gmail.com>
2024-11-24 22:07:30 -0500

    archivo db sql actualizado

--- base_de_datos/gestor_de_comisiones.sql
+++ base_de_datos/gestor_de_comisiones.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 24-11-2024 a las 19:34:41
+-- Tiempo de generación: 25-11-2024 a las 03:06:59
 -- Versión del servidor: 8.3.0
 -- Versión de PHP: 8.2.18
 
@@ -61,7 +61,7 @@ CREATE TABLE IF NOT EXISTS `bonificaciones` (
   `fecha_asignacion` date NOT NULL,
   PRIMARY KEY (`id`),
   KEY `identificacion` (`identificacion`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Volcado de datos para la tabla `bonificaciones`
@@ -70,7 +70,9 @@ CREATE TABLE IF NOT EXISTS `bonificaciones` (
 INSERT INTO `bonificaciones` (`id`, `identificacion`, `motivo`, `monto`, `fecha_asignacion`) VALUES
 (1, '1117016370', 'Bonificación por cumplimiento de meta en 2024-01', 300000.00, '2024-11-23'),
 (2, '123456789', 'Bonificación por cumplimiento de meta en 2024-01', 300000.00, '2024-11-23'),
-(3, '123456789', 'por lindo', 5000000.00, '2024-11-23');
+(3, '123456789', 'por lindo', 5000000.00, '2024-11-23'),
+(4, '1231232123', 'Bonificación por cumplimiento de meta en 2024-02', 300000.00, '2024-11-24'),
+(5, '1117016370', 'Bonificación por cumplimiento de meta en 2024-02', 300000.00, '2024-11-24');
 
 -- --------------------------------------------------------
 
@@ -87,18 +89,16 @@ CREATE TABLE IF NOT EXISTS `metas_ventas` (
   `ventas_actuales` decimal(10,2) DEFAULT '0.00',
   `cumplida` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`id`),
-  KEY `identificacion` (`identificacion`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  UNIQUE KEY `uc_identificacion_periodo` (`identificacion`,`periodo`)
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Volcado de datos para la tabla `metas_ventas`
 --
 
 INSERT INTO `metas_ventas` (`id`, `identificacion`, `periodo`, `meta`, `ventas_actuales`, `cumplida`) VALUES
-(1, '1117016370', '2024-01', 5000.00, 5000.00, 1),
-(2, '123456789', '2024-01', 5000.00, 16000.00, 1),
-(3, '123456789', '2024-01', 80000.00, 16000.00, 0),
-(4, '123456789', '2024-02', 16000.00, 8000.00, 0);
+(7, '1117016370', '2024-01', 5000.00, 40000.00, 1),
+(8, '1117016370', '2024-02', 10000.00, 50000.00, 1);
 
 -- --------------------------------------------------------
 
@@ -119,19 +119,15 @@ CREATE TABLE IF NOT EXISTS `reportes_desempeno` (
   `ausencias` int DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `identificacion` (`identificacion`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Volcado de datos para la tabla `reportes_desempeno`
 --
 
 INSERT INTO `reportes_desempeno` (`id`, `identificacion`, `periodo`, `ventas_totales`, `metas_cumplidas`, `bonificaciones_totales`, `porcentaje_cumplimiento`, `dias_trabajados`, `ausencias`) VALUES
-(1, '1117016370', '2024-01', 5000.00, 1, 0.00, 100.00, 1, 0),
-(2, '123456789', '2024-01', 16000.00, 1, 0.00, 50.00, 1, 1),
-(3, '123456789', '2024-03', 8000.00, 0, 0.00, 0.00, 0, 0),
-(4, '1117016370', '2024-02', 8000.00, 0, 0.00, 0.00, 0, 0),
-(5, '123456789', '2024-02', 8000.00, 0, 0.00, 0.00, 0, 0),
-(6, '1117016370', '2024-11', 516137.00, 0, 300000.00, 0.00, 0, 0);
+(1, '1117016370', '2024-02', 50000.00, 1, 0.00, 100.00, 0, 0),
+(2, '1117016370', '2024-01', 40000.00, 1, 0.00, 100.00, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -147,6 +143,7 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `nombre` varchar(100) NOT NULL,
   `fecha_registro` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `rol` enum('admin','empleado') NOT NULL DEFAULT 'empleado',
+  `activo` tinyint(1) DEFAULT '1',
   PRIMARY KEY (`id`),
   UNIQUE KEY `correo` (`correo`)
 ) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -155,9 +152,9 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `correo`, `contraseña`, `nombre`, `fecha_registro`, `rol`) VALUES
-(8, 'johan37021@gmail.com', '$2y$10$gtO6ViDEA2Gh5yYnQPL4juOX9iYyCzfNqqrxL954u/dED4QRSSBBi', 'Johan Alejandro Gómez Cifuentes', '2024-11-24 03:46:50', 'empleado'),
-(10, 'johan37021@admin.com', '$2y$10$bioAEMUnROvw1uj7Nl5aCuOQlgUsRgkzqJAS4GAvCNaskJqd52x2m', 'Johan Alejandro Gómez Cifuentes', '2024-11-24 04:32:00', 'admin');
+INSERT INTO `usuarios` (`id`, `correo`, `contraseña`, `nombre`, `fecha_registro`, `rol`, `activo`) VALUES
+(8, 'johan37021@gmail.com', '$2y$10$gtO6ViDEA2Gh5yYnQPL4juOX9iYyCzfNqqrxL954u/dED4QRSSBBi', 'Johan Alejandro Gómez Cifuentes', '2024-11-24 03:46:50', 'empleado', 1),
+(10, 'johan37021@admin.com', '$2y$10$bioAEMUnROvw1uj7Nl5aCuOQlgUsRgkzqJAS4GAvCNaskJqd52x2m', 'Johan Alejandro Gómez Cifuentes', '2024-11-24 04:32:00', 'admin', 1);
 
 -- --------------------------------------------------------
 
@@ -167,22 +164,21 @@ INSERT INTO `usuarios` (`id`, `correo`, `contraseña`, `nombre`, `fecha_registro
 
 DROP TABLE IF EXISTS `vendedores`;
 CREATE TABLE IF NOT EXISTS `vendedores` (
-  `identificacion` varchar(50) NOT NULL,
+  `identificacion` varchar(20) NOT NULL,
   `nombre` varchar(100) NOT NULL,
-  `correo` varchar(100) NOT NULL,
+  `correo` varchar(255) NOT NULL,
   `telefono` varchar(20) DEFAULT NULL,
-  `fecha_registro` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`identificacion`),
-  UNIQUE KEY `correo` (`correo`)
+  `activo` tinyint(1) DEFAULT '1',
+  PRIMARY KEY (`identificacion`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Volcado de datos para la tabla `vendedores`
 --
 
-INSERT INTO `vendedores` (`identificacion`, `nombre`, `correo`, `telefono`, `fecha_registro`) VALUES
-('1117016370', 'Johan Alejandro Gómez Cifuentes', 'johan.gomez@utp.edu.co', '3187477491', '2024-11-23 16:42:29'),
-('123456789', 'Cristian Giraldo Osorio', 'cristian.giraldo4@utp.edu.co', '1828128', '2024-11-23 20:13:43');
+INSERT INTO `vendedores` (`identificacion`, `nombre`, `correo`, `telefono`, `activo`) VALUES
+('1117016370', 'Johan Alejandro Gómez Cifuentes', 'johan37021@gmail.com', '2334788', 1),
+('1231232123', 'Cristian Giraldo Osorio', 'cristian.giraldo4@utp.edu.co', '2334788', 1);
 
 -- --------------------------------------------------------
 
@@ -199,26 +195,21 @@ CREATE TABLE IF NOT EXISTS `ventas` (
   `total` decimal(10,2) NOT NULL,
   `fecha_venta` date NOT NULL,
   `comision` decimal(10,2) DEFAULT NULL,
+  `activo` tinyint(1) DEFAULT '1',
   PRIMARY KEY (`id`),
   KEY `identificacion` (`identificacion`)
-) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=34 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Volcado de datos para la tabla `ventas`
 --
 
-INSERT INTO `ventas` (`id`, `identificacion`, `producto`, `cantidad`, `total`, `fecha_venta`, `comision`) VALUES
-(1, '1117016370', 'Tv', 1, 5000.00, '2024-01-08', 500.00),
-(2, '123456789', 'Gasimba', 1, 8000.00, '2024-01-10', 800.00),
-(3, '123456789', 'Gasimba', 1, 8000.00, '2024-01-11', 800.00),
-(4, '123456789', 'Gasimba', 1, 8000.00, '2024-03-11', 800.00),
-(5, '1117016370', 'Gasimba', 1, 8000.00, '2024-02-11', 800.00),
-(6, '123456789', 'Gasimba', 1, 8000.00, '2024-02-11', 800.00),
-(7, '1117016370', 'Gasimba', 1, 123231.00, '2024-11-24', 12323.10),
-(8, '1117016370', 'Gasimba', 1, 123231.00, '2024-11-19', 12323.10),
-(9, '1117016370', 'Gasimba', 1, 123231.00, '2024-11-27', 12323.10),
-(10, '1117016370', 'Gasimba', 1, 123231.00, '2024-11-27', 12323.10),
-(11, '1117016370', 'Gasimba', 1, 23213.00, '2024-11-27', 2321.30);
+INSERT INTO `ventas` (`id`, `identificacion`, `producto`, `cantidad`, `total`, `fecha_venta`, `comision`, `activo`) VALUES
+(33, '1117016370', 'Tv', 1, 50000.00, '2024-02-06', 5000.00, 1),
+(32, '1117016370', 'Tv', 1, 10000.00, '2024-01-02', 1000.00, 1),
+(30, '1117016370', 'Tv', 1, 10000.00, '2024-01-02', 1000.00, 1),
+(31, '1117016370', 'Tv', 1, 10000.00, '2024-01-02', 1000.00, 1),
+(29, '1117016370', 'Tv', 1, 10000.00, '2024-01-09', 1000.00, 1);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
